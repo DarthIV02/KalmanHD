590d68d8a3dc4f9cdaeea8c7d2e2cba7c5c9d9ce
DarthIV02 <ivannia.gomez@cetys.edu.mx>
2023-03-30 21:04:11 -0700

    Finish RegHD on .py

--- main.py
+++ main.py
@@ -4,40 +4,57 @@ import argparse
 import random
 import numpy as np
 from Stuff.DatasetLoader import DatasetLoader
+from Stuff.Initializer import Initializer
 
 def parse_option():
     parser = argparse.ArgumentParser('argument for training')
 
     parser.add_argument('--print_freq', type=int, default=10,
                         help='print frequency')
+    
     parser.add_argument('--epochs', type=int, default=1,
                         help='number of training epochs or number of passes on dataset')
+    
     parser.add_argument('--learning_rate', type=float, default=0.00001,
                         help='learning rate gradient for the model')
+    
     parser.add_argument('--hd_encoder', type=str, default='nonlinear',
                         choices=['nonlinear', 'time_encoding', 'bind_timeseries'],
                         help='the type of hd encoding function to use')
+    
     parser.add_argument('--clustering', type=str, default='none',
-                        choices=['none', 'spectral_clustering', 'k_means'],
+                        choices=['none', 'spectral_clustering', 'kmeans'],
                         help='the type of of clustering method to incorporate')
+    
     parser.add_argument('--models', type=int, default=1, 
                         help='When using clustering, the number of models to seperate the clustering')
-    parser.add_argument('--dimension', type=int, default=10000,
+    
+    parser.add_argument('--dimension_hd', type=int, default=10000,
                         help='number of dimensions in the hypervector')
+    
     parser.add_argument('--dataset', type=str, default='SanFranciscoTraffic', 
                         choices=['SanFranciscoTraffic', 'MetroInterstateTrafficVolume', 
                                  'GuangzhouTraffic', 'EnergyConsumptionFraunhofer', 'ElectricityLoadDiagrams'],
                         help='Dataset to initialize')
-    parser.add_argument('--num_timestamp_training', type=int, default=12, 
-                        help='Number of timestamps used for training over all the timeseries')
-    parser.add_argument('--num_timestamp_testing', type=int, default=3, 
-                        help='Number of timestamps used for testing over all the timeseries')
+    
+    parser.add_argument('--num_timestamp', type=int, default=15, 
+                        help='Number of timestamps used for training and testing over all the timeseries')
+    
     parser.add_argument('--trial', type=int, default=0,
                         help='id for recording multiple runs')
     
     parser.add_argument('--model', type=str, default='RegHD', 
                         choices=['RegHD', 'VAE', 'DNN'],
                         help='Model to test')
+    
+    parser.add_argument('--size_of_sample', type=int, default=20, 
+                        help='Number of previous samples before forecasting')
+    
+    parser.add_argument('--levels', type=int, default=6, 
+                        help='Number of levels to divide encoding when using a different hd-encoder')
+    
+    parser.add_argument('--retraining', type=bool, default=False, 
+                        help='If the model with this particular data, has been previously trained, set retraining = True')
     
     opt = parser.parse_args()
 
@@ -59,13 +76,23 @@ def main():
 
     matrix_1_original = dl.dataset_load_and_preprocess("original")
     matrix_1_norm = dl.dataset_load_and_preprocess("normalized")
+    sets = np.random.choice(matrix_1_norm.shape[1]-40, opt.num_timestamp, replace=False)
+    sets_training, sets_testing = sets[:int(len(sets)*.8)], sets[int(len(sets)*.8):]
 
     if opt.model == "RegHD":
+        from models.RegHD.RegHD import Return_Model
+        model = Return_Model(opt.size_of_sample, opt.dimension_hd, opt.models, opt)
+        model.train(sets_training, matrix_1_norm, opt.epochs)
+        model.test(sets_testing, matrix_1_norm)
 
-        from models.RegHD import RegHD
-        Trainer = BasicHD()
-        Trainer.start()
-        print("===RegHD Complete===")
+    if opt.model == "VAE":
+        from models.DNN import Return_Model, Train_Model, Test_Model
+        model = Return_Model(opt.size_of_sample)
+        Train_Model(model, matrix_1_norm, sets_training, opt.retraining, opt.dataset, opt.size_of_sample, opt.epochs)
+        Test_Model(model, matrix_1_norm, sets_testing, opt.size_of_sample)        
+
+    if opt.model == "DNN":
+        pass
 
 if __name__ == '__main__':
     main()
--- models/RegHD/RegHD.py
+++ models/RegHD/RegHD.py
@@ -1,5 +1,6 @@
 # For RegHD
 import math
+import types
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -7,119 +8,239 @@ import torch.utils.data as data
 import torchmetrics
 from tqdm import tqdm
 
-from torchhd import functional
-from torchhd import embeddings
-
 import numpy as np
 # Model based on RegHD application for Single model regression -> No comparing which cluster
 
+import math
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.utils.data as data
 
-class SingleModel_With_NonLinear_Sin(nn.Module):
-    def __init__(self, num_classes, size, d):
-        super(SingleModel_With_NonLinear_Sin, self).__init__()
+# Note: this example requires the torchmetrics library: https://torchmetrics.readthedocs.io
+from sklearn.metrics import mean_squared_error
+from tqdm import tqdm
 
-        self.lr = 0.00001  # alpha
-        self.M = torch.zeros(1, d).double()  # Model initializes in 0
-        # 5 features, 10000 dimensions = hypervectors like weights?
-        self.project = embeddings.Projection(size, d).double()
-        # Normal distributions mean=0.0, std=1.0
-        self.project.weight.data.normal_(0, 1)
+from torchhd import functional, embeddings, cos_similarity, bind, multiset, hard_quantize, permute
+
+from scipy.special import softmax
+
+from sklearn.cluster import KMeans, SpectralClustering
+
+import time
+
+# Model based on RegHD application for Single model regression -> No comparing which cluster
+class RegHD(nn.Module):
+    def __init__(self, size, d, models, **kwargs):
+        super(RegHD, self).__init__()
+
+        self.lr = kwargs['opt'].learning_rate # alpha
+        self.M = torch.zeros(models, d).double() # Model initializes in 0
+        self.project = embeddings.Projection(size, d).double() # 5 features, 10000 dimensions = hypervectors like weights?
+        self.project.weight.data.normal_(0, 1) # Normal distributions mean=0.0, std=1.0
         self.bias = nn.parameter.Parameter(torch.empty(d), requires_grad=False)
-        self.bias.data.uniform_(0, 2 * math.pi)  # bias
+        self.bias.data.uniform_(0, 2 * math.pi) # bias
+        self.cluster = functional.random_hv(models, d)
+        self.size = size
+        self.d = d
+        self.opt = kwargs['opt']
 
-    def encode(self, x):  # encoding a value
+    def encode(self, x): # encoding a value
         for i in range(len(x)):
             x[i] = float(x[i])
         enc = self.project(x)
-        sample_hv = torch.cos(enc + self.bias) * torch.sin(enc)
+        sample_hv = torch.cos(enc + self.bias) * torch.sin(enc) 
         return functional.hard_quantize(sample_hv)
 
-    def model_update(self, x, y):  # update # y = no hv
-        # Model + alpha*(Error)*(x)
-        update = self.M + self.lr * (y - (F.linear(x, self.M))) * x
-        # update = update.mean(0) # Mean by columns
-        self.M = update  # New
+    def model_update(self, x, y): # update # y = no hv
+        x = torch.reshape(x, (1,self.d))
+        confidence = np.transpose(softmax(cos_similarity(self.cluster, x))) # Compare input with cluster
+        model_result = F.linear(x.type(torch.FloatTensor), self.M.type(torch.FloatTensor))
+        update = self.M + (float(self.lr) * float(y - F.linear(confidence, model_result)) * x) # Model + alpha*(Error)*(x)
+        #update = update.mean(0) # Mean by columns
+        self.M = update # New 
+        # update cluster center?
+        center = [num.item() for num in confidence[0]].index(max(confidence[0]).item())
+        self.cluster[center] = self.cluster[center] + (1-max(confidence[0])) * x
+        return center
+        
 
     def forward(self, x):
-        enc = self.encode(x)
-        # Multiply enc (x) * weights (Model) = Dot product
-        res = F.linear(enc, self.M)
-        return res  # Return the resolutions
+        enc = torch.reshape(self.encode(x), (1, self.d))
+        confidence = np.transpose(softmax(cos_similarity(self.cluster, enc))) # Compare input with cluster
+        model_result = F.linear(enc, self.M.type(torch.FloatTensor))
+        res = F.linear(confidence, model_result) # Multiply enc (x) * weights (Model) = Dot product
+        return res # Return the resolutions
 
-
-def Return_Model():
-    model_hd = SingleModel_With_NonLinear_Sin(2, 40, 10000)  # 1 class, 5
-    return model_hd
-
-
-def Train_Model(model_hd, matrix, sets_training):
-    time_arr = []
-    # main()
-    # print("--- %s seconds ---" % (time.time() - start_time))
-
-    with torch.no_grad():  # disabled gradient calculation because were doing it manually
-        for _ in tqdm(range(10)):  # Number of iterations for all the samples
-            mse = torchmetrics.MeanSquaredError()
-            for i in sets_training:  # Calculated at random
-                pred = []
-                samples = matrix[:, i:i+40]
-                labels = matrix[:, i+40]
-                # print(f"sample: {samples}, label:{labels}")
-                # samples = samples.to(device) # pass sample and label (1 at a time)
-                # labels = labels.to(device)
-
+    def train(self, sets_training, matrix_1_norm, epochs):
+        for _ in range(epochs): # Number of iterations for all the samples
+            pred = []
+            labels_full = []
+            for i in tqdm(sets_training):
+                samples = matrix_1_norm[:, i:i+self.size]
+                labels = matrix_1_norm[:, i+self.size]
+                
                 for n in range(samples.shape[0]):
                     label = torch.tensor(labels[n])
                     sample = torch.tensor(samples[n, :])
-                    # samples =
-                    samples_hv = model_hd.encode(sample)  # Encode the inputs
-                    # print(f"sample_hv:{samples_hv}")
-                    # Pass input and label to train
-                    model_hd.model_update(samples_hv, label)
+                    samples_hv = self.encode(sample) # Encode the inputs
+                    self.model_update(samples_hv, label) # Pass input and label to train
+                    predictions_testing = self.forward(sample) # Pass samples from test to model (forward function)
+                    pred.append(float(predictions_testing[0]))
+                    labels_full.append(float(label.unsqueeze(dim=0)))
 
-                    # Pass samples from test to model (forward function)
-                    predictions_testing = model_hd(sample)
-                    # print(predictions_testing.item())
-                    pred.append(predictions_testing)
-                    # predictions_testing = predictions_testing
-                    # label = label
-                    mse.update(predictions_testing, label.unsqueeze(dim=0))
-                    # mse.update(predictions_testing.cpu(), label)
-
-                    # time_arr.append((time.time() - start_time, np.absolute(np.sum(labels-pred))))
-                    time_arr.append(np.absolute(
-                        label.item()-predictions_testing.item()))
-
-            print(
-                f"Training mean squared error of {(mse.compute().item()):.3f}")
-
-    return model_hd, time_arr
-
-
-def Test_Model(model_hd, matrix, sets_testing):
-    pred_hd = []
-    original_hd = []
-
-    with torch.no_grad():
-        mse = torchmetrics.MeanSquaredError()
+            print(f"Training root mean squared error of {(mean_squared_error(labels_full, pred, squared=False)):.3f}")
+    
+    def test(self, sets_testing, matrix_1_norm):
+        pred = []
+        labels_full = []
         for i in tqdm(sets_testing):
-            pred = []
-            samples = matrix[:, i:i+40]
-            labels = matrix[:, i+40]
+            samples = matrix_1_norm[:, i:i+self.size]
+            labels = matrix_1_norm[:, i+self.size]
             for n in range(samples.shape[0]):
                 label = torch.tensor(labels[n])
                 sample = torch.tensor(samples[n, :])
                 # Pass samples from test to model (forward function)
-                predictions = model_hd(sample)
-                pred.append(predictions)
-                # predictions = predictions * TARGET_STD + TARGET_MEAN # What is target
-                # labels = labels * TARGET_STD + TARGET_MEAN
-                mse.update(predictions, label.unsqueeze(dim=0))
-                # dif_hd.append(np.absolute(label-predictions))
-                pred_hd.append(predictions)
-                original_hd.append(label)
+                predictions = self.forward(sample)
+                pred.append(float(predictions[0]))
+                labels_full.append(float(label.unsqueeze(dim=0)))
 
         print(
-            f"Testing mean squared error of testing {(mse.compute().item()):.3f}")
+            f"Testing root mean squared error of testing {(mean_squared_error(labels_full, pred, squared=False)):.3f}")
 
-    return model_hd, pred_hd, original_hd
+def bind_timeseries(self, x): # encoding a value
+        for i in range(len(x)):
+            x[i] = float(x[i])
+        sample_hv = bind(self.position.weight, self.value(x))
+        sample_hv = multiset(sample_hv)
+        return hard_quantize(sample_hv)
+
+def time_encoding(self, x): # encoding a value
+        for i in range(len(x)):
+            x[i] = float(x[i])
+        sample_hv = bind(self.position.weight, self.value(x))
+        for i, hv in enumerate(sample_hv):
+            sample_hv[i] = permute(hv, shifts=i)
+        sample_hv = multiset(sample_hv)
+        return hard_quantize(sample_hv)
+
+class RegHD_Kmeans(RegHD):
+    def __init__(self, size, d, models, **kwargs):
+        super().__init__(size, d, models, **kwargs)   
+        self.cluster_model = None
+        self.models = models
+
+    def model_update(self, x, y, cluster): # update # y = no hv
+        x = torch.reshape(x, (1,self.d))
+        model_result = F.linear(x, self.M[cluster]).type(torch.FloatTensor)
+        update = self.M[cluster] + (self.lr * (y - model_result) * x) # Model + alpha*(Error)*(x)
+        self.M[cluster] = update[0] # New 
+    
+    def forward(self, x):
+        enc = torch.reshape(self.encode(x), (1,self.d))
+        model_result = F.linear(enc, self.M[self.cluster_model.predict(enc)])
+        return model_result # Return the resolutions
+    
+    def train(self, sets_training, matrix_1_norm, epochs):
+        
+        full_samples = []
+        for i in sets_training:
+            samples = matrix_1_norm[:, i:i+self.size]
+            for n in range(samples.shape[0]):
+                sample = torch.tensor(samples[n, :])
+                full_samples.append(sample)
+        
+        encoded = [self.encode(samp).numpy() for samp in full_samples]
+        
+        print("Kmeans Clusterization Started")
+        
+        if(self.cluster_model == None):
+            kmeans = KMeans(n_clusters=self.models, random_state=0, n_init=10).fit(encoded)
+            self.cluster_model = kmeans
+
+        for _ in range(epochs): # Number of iterations for all the samples
+            pred = []
+            labels_full = []
+            for time, i in tqdm(enumerate(sets_training)):
+                labels = matrix_1_norm[:, i+self.size]
+                
+                for n in range(samples.shape[0]):
+                    label = torch.tensor(labels[n])
+                    item = (samples.shape[0]*time) + n
+                    self.model_update(torch.tensor(encoded[item]), label, self.cluster_model.labels_[item]) # Pass input and label to train
+                    predictions_testing = self(full_samples[item]) # Pass samples from test to model (forward function)
+                    pred.append(float(predictions_testing[0]))
+                    labels_full.append(float(label.unsqueeze(dim=0)))
+
+            print(f"Training root mean squared error of {(mean_squared_error(labels_full, pred, squared=False)):.3f}")
+
+class RegHD_SpectralClustering (RegHD):
+    def __init__(self, size, d, models, **kwargs):
+        super().__init__(size, d, models, **kwargs)
+        self.cluster_model = None
+        self.training_samples = None
+        self.models = models
+
+    def model_update(self, x, y, cluster): # update # y = no hv
+        x = torch.reshape(x, (1,self.d))
+        model_result = F.linear(x.type(torch.FloatTensor), self.M[cluster].type(torch.FloatTensor))
+        update = self.M[cluster] + (self.lr * (y - model_result) * x) # Model + alpha*(Error)*(x)
+        self.M[cluster] = update[0] # New 
+
+    def forward(self, x):
+        enc = torch.reshape(self.encode(x), (1,self.d))
+        similarity = cos_similarity(self.training_samples, enc)
+        i = list(similarity.numpy()).index(max(similarity).numpy()[0])
+        model_result = F.linear(enc, self.M[self.cluster_model[i]])
+        return model_result # Return the resolutions
+    
+    def train(self, sets_training, matrix_1_norm, epochs):
+        
+        full_samples = {}
+        for time, i in enumerate(sets_training):
+            samples = matrix_1_norm[:, i:i+self.size]
+            for n in range(0, samples.shape[0], 1):
+                sample = torch.tensor(samples[n, :])
+                full_samples[(samples.shape[0]*time) + n] = sample
+
+        if(self.cluster_model == None):
+            encoded = torch.zeros(len(full_samples), self.d)
+            for i, samp in (full_samples.items()):
+                encoded[i, :] = self.encode(samp)
+            self.training_samples = encoded
+            affinity_matrix = cos_similarity(encoded, encoded)
+            clustering = SpectralClustering(n_clusters=self.models, assign_labels='discretize', 
+                                            random_state=0, affinity='precomputed').fit(affinity_matrix)
+            self.cluster_model = clustering.labels_
+
+        for _ in range(epochs): # Number of iterations for all the samples
+            for time, i in enumerate(sets_training):
+                labels = matrix_1_norm[:, i+self.size]
+                
+                for n in range(samples.shape[0]):
+                    label = torch.tensor(labels[n])
+                    item = (samples.shape[0]*time) + n
+                    self.model_update(encoded[item], label, self.cluster_model[item]) # Pass input and label to train
+
+def Return_Model(size, d, models, opt):
+
+    if(opt.clustering == "none"):
+        model_hd = RegHD(size, d, models, opt = opt)  # 1 class, 5
+
+    elif(opt.clustering == "kmeans"):
+        model_hd = RegHD_Kmeans(size, d, models, opt = opt)
+
+    elif(opt.clustering == "spectral_clustering"):
+        model_hd = RegHD_SpectralClustering(size, d, models, opt = opt)
+
+    model_hd.lr = opt.learning_rate
+
+    if(opt.hd_encoder != "nonlinear"):
+        model_hd.position = embeddings.Random(size, d)
+        model_hd.value = embeddings.Level(opt.levels, d)
+        if (opt.hd_encoder == "bind_timeseries"):
+            model_hd.encode = types.MethodType(bind_timeseries, model_hd)
+        elif(opt.hd_encoder == "time_encoding"):
+            model_hd.encode = types.MethodType(time_encoding, model_hd)
+
+    return model_hd
