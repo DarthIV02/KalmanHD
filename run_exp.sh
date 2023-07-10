54455c604dfca396592f8ae84df7429765bee8ca
DarthIV02 <ivannia.gomez@cetys.edu.mx>
2023-07-09 21:44:43 -0700

    Update run_exp.sh

--- run_exp.sh
+++ run_exp.sh
@@ -4,13 +4,22 @@ do
 
     # python3 main.py --model KalmanFilter --dataset "$dataset";
     
-    for hd_representation in 1 2 4 8
+    for hd_representation in 1 2 4
     do
         # for learning_rate in 0.1 0.01 0.001 0.0001 0.00001
         # do
         #     python3 main.py --dataset "$dataset" --learning_rate "$learning_rate" --hd_representation "$hd_representation"; 
         # done
 
-        python3 main.py --learning_rate 0.00001 --dimension_hd 10000 --model RegHD --dataset "$dataset" --hd_representation "$hd_representation";
+        if [ "$hd_representation" = 1 ]; then
+            learning_rate=0.00001
+        elif [ "$hd_representation" = 2 ]; then
+            learning_rate=0.00001
+        elif [ "$hd_representation" = 4 ]; then
+            learning_rate=0.000001
+        fi
+
+        python3 main.py --learning_rate "$learning_rate" --dimension_hd 10000 --model RegHD --dataset "$dataset" --hd_representation "$hd_representation";
+    
     done
 done
