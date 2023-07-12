eb2fbe334005f097ad5f81dc2026c51579bb46b5
DarthIV02 <ivannia.gomez@cetys.edu.mx>
2023-07-12 15:36:43 -0700

    Exp gaussian again
    Dimension correction

--- run_exp_gaussian.sh
+++ run_exp_gaussian.sh
@@ -24,7 +24,7 @@ do
             dimension_hd=5000
         fi
         
-        python3 main.py --model RegHD --dataset "$dataset" --learning_rate "$learning_rate" --hd_representation "$hd_representation" --gaussian_noise "$noise";
+        python3 main.py --model RegHD --dataset "$dataset" --learning_rate "$learning_rate" --hd_representation "$hd_representation" --dimension_hd "$dimension_hd" --gaussian_noise "$noise";
 
         # python3 main.py --model KalmanFilter --dataset "$dataset" --gaussian_noise "$noise";
         
