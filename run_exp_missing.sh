fd44d4fd14c93757007b7cd6b1ac3cc97a82f5eb
DarthIV02 <ivannia.gomez@cetys.edu.mx>
2023-07-12 15:10:14 -0700

    Exp Missing RegHD

--- run_exp_missing.sh
+++ run_exp_missing.sh
@@ -2,9 +2,31 @@ for noise in 0.1 0.2 0.4 0.5
 do
     for dataset in SanFranciscoTraffic MetroInterstateTrafficVolume GuangzhouTraffic EnergyConsumptionFraunhofer ElectricityLoadDiagrams
     do
-        python3 main.py --learning_rate 0.00001 --dimension_hd 10000 --model RegHD --dataset "$dataset" --p "$noise";
+        if [ "$dataset" = "SanFranciscoTraffic" ]; then
+            learning_rate=0.000001
+            hd_representation=4
+            dimension_hd=2000
+        elif [ "$dataset" = "MetroInterstateTrafficVolume" ]; then
+            learning_rate=0.00001
+            hd_representation=2
+            dimension_hd=1000
+        elif [ "$dataset" = "GuangzhouTraffic" ]; then
+            learning_rate=0.000001
+            hd_representation=4
+            dimension_hd=2000
+        elif [ "$dataset" = "EnergyConsumptionFraunhofer" ]; then
+            learning_rate=0.000001
+            hd_representation=4
+            dimension_hd=2000
+        elif [ "$dataset" = "ElectricityLoadDiagrams" ]; then
+            learning_rate=0.00001
+            hd_representation=1
+            dimension_hd=5000
+        fi
+        
+        python3 main.py --model RegHD --dataset "$dataset" --learning_rate "$learning_rate" --hd_representation "$hd_representation" --p "$noise";
 
-        python3 main.py --model KalmanFilter --dataset "$dataset" --p "$noise";
+        # python3 main.py --model KalmanFilter --dataset "$dataset" --p "$noise";
         
         if [ "$dataset" = "SanFranciscoTraffic" ]; then
             learning_rate=0.01
@@ -23,6 +45,6 @@ do
             hd_representation=1
         fi
 
-        python3 main.py --dataset "$dataset" --learning_rate "$learning_rate" --hd_representation "$hd_representation" --p "$noise";
+        # python3 main.py --dataset "$dataset" --learning_rate "$learning_rate" --hd_representation "$hd_representation" --p "$noise";
     done
 done
