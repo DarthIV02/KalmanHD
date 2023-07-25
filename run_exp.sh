47e92be486cf7f2c6d7850a483fe6b4d57934234
DarthIV02 <ivannia.gomez@cetys.edu.mx>
2023-07-24 18:28:14 -0700

    Comment single model

--- run_exp.sh
+++ run_exp.sh
@@ -1,30 +1,26 @@
 for dataset in SanFranciscoTraffic MetroInterstateTrafficVolume GuangzhouTraffic EnergyConsumptionFraunhofer ElectricityLoadDiagrams
 do
-    # python3 main.py --learning_rate 0.00001 --dimension_hd 10000 --model RegHD --dataset "$dataset";
 
-    # python3 main.py --model KalmanFilter --dataset "$dataset";
+    python3 main.py --model KalmanFilter --dataset "$dataset";
     
-    #for hd_representation in 1 2 4
-    #do
-        # for learning_rate in 0.1 0.01 0.001 0.0001 0.00001
-        # do
-        #     python3 main.py --dataset "$dataset" --learning_rate "$learning_rate" --hd_representation "$hd_representation"; 
-        # done
-        
-    #    for dimension_hd in 500 1000 2000 5000 10000
-    #    do
-    #        if [ "$hd_representation" = 1 ]; then
-    #        learning_rate=0.00001
-    #        elif [ "$hd_representation" = 2 ]; then
-    #            learning_rate=0.00001
-    #        elif [ "$hd_representation" = 4 ]; then
-    #            learning_rate=0.000001
-    #        fi
+    if [ "$dataset" = "SanFranciscoTraffic" ]; then
+        learning_rate=0.01
+        hd_representation=4
+    elif [ "$dataset" = "MetroInterstateTrafficVolume" ]; then
+        learning_rate=0.1
+        hd_representation=2
+    elif [ "$dataset" = "GuangzhouTraffic" ]; then
+        learning_rate=0.01
+        hd_representation=4
+    elif [ "$dataset" = "EnergyConsumptionFraunhofer" ]; then
+        learning_rate=0.01
+        hd_representation=4
+    elif [ "$dataset" = "ElectricityLoadDiagrams" ]; then
+        learning_rate=0.01
+        hd_representation=1
+    fi
 
-    #        python3 main.py --learning_rate "$learning_rate" --dimension_hd "$dimension_hd" --model RegHD --dataset "$dataset" --hd_representation "$hd_representation";
-
-    #    done    
-    #done
+    python3 main.py --model KalmanHD --dataset "$dataset" --learning_rate "$learning_rate" --hd_representation "$hd_representation";
 
     if [ "$dataset" = "SanFranciscoTraffic" ]; then
         learning_rate=0.000001
@@ -48,10 +44,10 @@ do
         dimension_hd=5000
     fi
     
-    #python3 main.py --learning_rate "$learning_rate" --dimension_hd "$dimension_hd" --model RegHD --dataset "$dataset" --hd_representation "$hd_representation";
+    python3 main.py --learning_rate "$learning_rate" --dimension_hd "$dimension_hd" --model RegHD --dataset "$dataset" --hd_representation "$hd_representation";
 
     python3 main.py --model DNN --dataset "$dataset";
 
-    #python3 main.py --model VAE --dataset "$dataset";
+    python3 main.py --model VAE --dataset "$dataset";
 
 done
