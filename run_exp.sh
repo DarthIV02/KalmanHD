9c13fea3fca28d8e39db58d9d7e5c3ee9a93ffbb
DarthIV02 <ivannia.gomez@cetys.edu.mx>
2023-08-03 19:54:08 -0700

    Update the learning rate

--- run_exp.sh
+++ run_exp.sh
@@ -1,27 +1,22 @@
-for dataset in MetroInterstateTrafficVolume GuangzhouTraffic EnergyConsumptionFraunhofer ElectricityLoadDiagrams # SanFranciscoTraffic
+for dataset in MetroInterstateTrafficVolume GuangzhouTraffic EnergyConsumptionFraunhofer ElectricityLoadDiagrams SanFranciscoTraffic
 do
-    python3 main.py --model KalmanHD --dataset "$dataset";
+    #python3 main.py --model KalmanHD --dataset "$dataset";
+    (( time python3 main.py --model KalmanFilter --dataset "$dataset" ) 2>&1 ) | tee KalmanFilter_"$dataset";
 
-    # python3 main.py --model KalmanFilter --dataset "$dataset";
-    
+
     if [ "$dataset" = "SanFranciscoTraffic" ]; then
-        learning_rate=0.01
-        hd_representation=4
+        learning_rate=0.001
     elif [ "$dataset" = "MetroInterstateTrafficVolume" ]; then
-        learning_rate=0.1
-        hd_representation=2
+        learning_rate=1e-05
     elif [ "$dataset" = "GuangzhouTraffic" ]; then
-        learning_rate=0.01
-        hd_representation=4
+        learning_rate=0.001
     elif [ "$dataset" = "EnergyConsumptionFraunhofer" ]; then
-        learning_rate=0.01
-        hd_representation=4
+        learning_rate=0.001
     elif [ "$dataset" = "ElectricityLoadDiagrams" ]; then
-        learning_rate=0.01
-        hd_representation=1
+        learning_rate=0.0001
     fi
 
-    python3 main.py --model KalmanHD --dataset "$dataset" --learning_rate "$learning_rate" --hd_representation "$hd_representation";
+    (( time python3 main.py --model KalmanHD --dataset "$dataset" --learning_rate "$learning_rate" --hd_representation "$hd_representation" ) 2>&1 ) | tee KalmanHD_"$dataset";
 
     if [ "$dataset" = "SanFranciscoTraffic" ]; then
         learning_rate=0.000001
@@ -44,11 +39,11 @@ do
         hd_representation=1
         dimension_hd=5000
     fi
-    
-    #python3 main.py --learning_rate "$learning_rate" --dimension_hd "$dimension_hd" --model RegHD --dataset "$dataset" --hd_representation "$hd_representation";
 
-    # python3 main.py --model DNN --dataset "$dataset";
+    (( time python3 main.py --learning_rate "$learning_rate" --dimension_hd "$dimension_hd" --model RegHD --dataset "$dataset" --hd_representation "$hd_representation" ) 2>&1 ) | tee RegHD_"$dataset";
 
-    #python3 main.py --model VAE --dataset "$dataset";
+    (( time python3 main.py --model DNN --dataset "$dataset" ) 2>&1 ) | tee DNN_"$dataset";
 
+    (( time python3 main.py --model VAE --dataset "$dataset" ) 2>&1 ) | tee VAE_"$dataset";
 done
+
