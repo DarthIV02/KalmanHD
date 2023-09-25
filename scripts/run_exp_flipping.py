b5501f669b15b880d8d71b9e5b8f63e7fbec6066
DarthIV02 <ivannia.gomez@cetys.edu.mx>
2023-09-25 11:17:29 -0700

    Delete flipping experiments

--- scripts/run_exp_flipping.py
+++
@@ -1,5 +0,0 @@
-import subprocess
-
-for noise in [0.01, 0.02, 0.05]:
-    for dataset in ["SanFranciscoTraffic", "MetroInterstateTrafficVolume", "GuangzhouTraffic", "EnergyConsumptionFraunhofer", "ElectricityLoadDiagrams"]:
-        subprocess.call(f"python main.py --model DNN --dataset {dataset} --flipping_rate {noise}", shell=True)
